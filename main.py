16d141cb52d40f1ace47de7c0bbb9e1b7517ac95
岸田星道 <kishiri@arayanoMacBook-Pro.local>
2022-07-08 15:31:54 +0900

    add dataloader

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ import tqdm
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 
+train_imgs = ImageFolder(
+    "taco_and_burrito/train",
+    transform=transforms.Compose([
+        transforms.RandomCrop(224),
+        transforms.ToTensor()
+    ])
+)
+test_imgs = ImageFolder(
+    "taco_and_burrito/test",
+    transform=transforms.Compose([
+        transforms.RandomCrop(224),
+        transforms.ToTensor()
+    ])
+)
+
+#DataLoaderを作成
+train_loader = DataLoader(
+    train_imgs, batch_size=32, shuffle=True
+)
+test_loader = DataLoader(
+    test_imgs, batch_size=32, shuffle=False
+)
